780c9505080dd6785d21d4f8c9cbb951ed97aa82
Henri Bergius <henri.bergius@iki.fi>
2024-11-13 11:19:24 +0000

    Switch order of operations

--- .github/backup.sh
+++ .github/backup.sh
@@ -4,12 +4,12 @@ cd /home/pi/log
 git pull
 echo "Backing up digital logbook"
 cp /home/pi/.signalk/plugin-config-data/signalk-logbook/* _data/logbook/
-git add _data/logbook/*.yml
 echo "Producing GeoJson track files"
 node .github/updatetracks.mjs
-git add tracks/*.json
 echo "Committing changes as needed"
 if [ -z "$(git status --porcelain)" ]; then exit 0;fi
+git add _data/logbook/*.yml
+git add tracks/*.json
 git config --local user.email boat@lille-oe.de
 git config --local user.name Lille Oe
 git commit -m "Backup logbook entries"
