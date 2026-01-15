7cc2ae9076fda8ca02d8c4fd591c8ae206701ccc
Henri Bergius <henri.bergius@iki.fi>
2026-01-15 15:38:09 -0500

    Copy synced log entries

--- .github/backup.sh
+++ .github/backup.sh
@@ -4,6 +4,8 @@ cd /home/pi/log
 git pull origin main
 echo "Backing up digital logbook"
 cp /home/pi/.signalk/plugin-config-data/signalk-logbook/* _data/logbook/
+cp /home/pi/log-sync/_logs/*.md _logs/
+cp "/home/pi/log-sync/$(date +%Y)/* $(date +%Y)"
 echo "Producing GeoJson track files"
 node .github/updatetracks.mjs
 echo "Committing changes as needed"
@@ -11,7 +13,7 @@ if [ -z "$(git status --porcelain)" ]; then exit 0;fi
 git add _data/logbook/*.yml
 git add tracks/*.json
 git add _logs/*.md
-git add "$(date +%Y)/*.jpeg"
+git add "$(date +%Y)/*"
 git config --local user.email boat@lille-oe.de
 git config --local user.name Lille Oe
 git commit -m "Backup logbook entries"
